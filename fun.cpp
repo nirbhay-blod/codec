bf333690afce56b3e520a36b61b8c6ec25d6fa1b
nirbhay97 <knirbhay805@gmail.com>
2023-06-21 08:18:58 +0530

    templates.
    Co-authored-by: nirbhay97 <knirbhay805@gmail.com>
    Co-authored-by: nirbhay-blod <nirbhayk@blod.in>

--- fun.cpp
+++ fun.cpp
@@ -1,7 +1,43 @@
 
-int main(){
-  int n;
-  cin >> n;
-  cout << n << endl;
-  return 0;
+// dsu code
+
+vector<int>parent;
+int findparent(int node){
+   if(node == parent[node]) return node;
+   return parent[node] = findparent(parent[node]);
 }
+void Union(int u, int v){
+   int x = findparent(u);
+   int y = findparent(v);
+   if(x == y) return;
+   parent[x] = y;
+}
+
+
+// temp
+#include <bits/stdc++.h>
+#define int long long 
+using namespace std;
+//Nirbhay kumar
+
+
+void solve(){
+}
+
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
+/* Hey you should check this out
+* int overflow, array bounds
+* reset global array and variable
+* look for special cases (n=1?)
+* do something instead of nothing and stay organized
+* bruteforce to find pattern
+* DON'T GET STUCK ON ONE APPROACH*/
